b2a39f04266259769c86ee92d615789a94125768
huangkun <jack-huang-developer@foxmail.com>
2017-01-26 21:44:52 +0800

    Remove UI-testing pod.

--- Example/Pods/Target Support Files/AFTPhotoScroller/AFTPhotoScroller-umbrella.h
+++ Example/Pods/Target Support Files/AFTPhotoScroller/AFTPhotoScroller-umbrella.h
@@ -4,6 +4,7 @@
 
 #import "AFTImageScrollView.h"
 #import "AFTPagingScrollView.h"
+#import "AFTPhotoScroller.h"
 
 FOUNDATION_EXPORT double AFTPhotoScrollerVersionNumber;
 FOUNDATION_EXPORT const unsigned char AFTPhotoScrollerVersionString[];
